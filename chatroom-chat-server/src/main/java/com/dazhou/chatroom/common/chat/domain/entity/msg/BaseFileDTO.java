13dbc8aafd1a5b8c55ddc9b4784005fe17fce539
Dazhou-del <1797050390@qq.com>
2024-02-03 23:49:12 +0800

    创建消息文件框架

---
+++ chatroom-chat-server/src/main/java/com/dazhou/chatroom/common/chat/domain/entity/msg/BaseFileDTO.java
@@ -0,0 +1,8 @@
+package com.dazhou.chatroom.common.chat.domain.entity.msg;
+
+/**
+ * @author <a href="https://github.com/Dazhou-del">Dazhou</a>
+ * @create 2024-02-03 23:47
+ */
+public class BaseFileDTO {
+}
